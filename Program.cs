894a6dc01a7506adda1f4b30b3a99992298a0fcc
donneyo <donneyo63@gmail.com>
2024-08-19 04:21:43 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task_3;
+
+class Program
+{
+    static void Main(string[] args)
+     {
+            // Use a for loop to iterate from 1 to 10
+            for (int i = 1; i <= 10; i++)
+            {
+                // Print each number to the console
+                Console.WriteLine(i);
+            }
+
+            // Keep the console window open
+            Console.ReadLine();
+        }
+}
